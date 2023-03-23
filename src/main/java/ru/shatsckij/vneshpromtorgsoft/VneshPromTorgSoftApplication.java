ccd501571b7c7a8a95bbf95a2876ebbe0803a86f
Shatsckij <shatsckij.i@yandex.ru>
2023-03-23 22:40:26 +0300

    Init

---
+++ src/main/java/ru/shatsckij/vneshpromtorgsoft/VneshPromTorgSoftApplication.java
@@ -0,0 +1,13 @@
+package ru.shatsckij.vneshpromtorgsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VneshPromTorgSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VneshPromTorgSoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shatsckij/vneshpromtorgsoft/VneshPromTorgSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shatsckij.vneshpromtorgsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VneshPromTorgSoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
